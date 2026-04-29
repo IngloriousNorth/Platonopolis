e0833c0d0f7b8a6c3e6d3040d66023b4573a88b3
Selapian <christianjosiahperrone@gmail.com>
2026-04-29 18:00:29 -0400

    Delete static/client/MODEL directory

--- static/client/MODEL/torrentsModel.js
+++
@@ -1,9 +0,0 @@
-var tableData;
-
-function insertTableData(data){
-	tableData = data;
-}
-
-var types = ["Nonfiction", "Fiction", "Holy Book", "Classical Music", "Short Story", "Poetry", "Essay", "Journal Article" ]
-var media = ["Ebook", "Audiobook", "Single", "Album"]
-var formats = ["PDF", "mp3"]
