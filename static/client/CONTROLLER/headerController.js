a458c229ebca1fefe1255de80b72ac2c88fae236
Selapian <christianjosiahperrone@gmail.com>
2026-04-29 18:00:02 -0400

    Delete static/client/CONTROLLER directory

--- static/client/CONTROLLER/headerController.js
+++
@@ -1,6 +0,0 @@
-function DMCA(){
-	$(".DMCA").click(function(e){
-		e.preventDefault();
-		alert("Send DMCA complaints to: inevitableambrosia@gmail.com. Please note that what we archive and seed are PDFs and audiobooks that show up on the first few pages of a Google™ search.")
-	})
-}
