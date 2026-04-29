f11ca737d92a7350e9eb65da0adf6c92fa61fa83
Selapian <christianjosiahperrone@gmail.com>
2026-04-29 18:00:36 -0400

    Delete static/client/VIEW directory

--- static/client/VIEW/mobile.js
+++
@@ -1,8 +0,0 @@
-$(document).on("click", "#mobile_menu", function(e){
-	e.preventDefault()
-	$(".mobile_menu").slideToggle();
-})
-
-$(document).on("click", "a.TEMPLAR", function(e){
-	$(".mobile_menu").slideUp()
-})
