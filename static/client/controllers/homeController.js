f83419eadc8b59208cb7043e7ac2bf7e72e7a0a4
Selapian <christianjosiahperrone@gmail.com>
2026-05-01 14:46:08 -0400

    Delete static/client/controllers directory

--- static/client/controllers/homeController.js
+++
@@ -1,6 +0,0 @@
-function initializeHome(){
-	$.get("/snatches", function(data){ 
-		$("#num_torrents_stat").text(numberWithCommas(data.torrents));
-		$("#num_snatches_stat").text(numberWithCommas(data.snatches)); 
-	})	
-} 
