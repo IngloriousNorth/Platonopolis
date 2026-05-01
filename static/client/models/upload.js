2cdd6cab240cf4fd747e757ba6eeebc4965503c6
Selapian <christianjosiahperrone@gmail.com>
2026-05-01 14:47:10 -0400

    Add files via upload

---
+++ static/client/models/upload.js
@@ -0,0 +1,21 @@
+var uploadModel = {
+	authors : [],
+	classes : [],
+	torrent : {
+		size : "",
+		release : "",
+		media : "",
+		format : "",
+		res : ""
+	},
+	edition: {
+		edition_date : "",
+		edition_title : "",
+		edition_publisher : "",
+		edition_no : "",
+		edition_pages : ""
+
+	},
+	editions : [],
+	title : ""
+}
