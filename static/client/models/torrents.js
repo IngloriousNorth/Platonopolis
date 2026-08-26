e39e662d9281e4b53ce3bff05cb1bfda7bc3dadd
IngloriousNorth <christianjosiahperrone@gmail.com>
2026-08-25 23:43:07 -0400

    Add files via upload

--- static/client/models/torrents.js
+++ static/client/models/torrents.js
@@ -13,6 +13,6 @@ function insertTableData(data){
 const types = ["Nonfiction", "Fiction", "Classical Music", "Renaissance Art", "Documentary", "Holy Book", "Codex", "Play", "Lecture", "Letter", "Encyclopedia", "Textbook", "Short Story", "Poetry", "Children Book", "Essay", "Journal Article", "Chant" ]
 const media = ["Ebook", "Audiobook", "Sheet Music", "Single", "Album", "EP", "Performance", "Canvas", "Sculpture", "Photography", "TV Episode", "Video Short", "Feature Film"]
 const formats = ["PDF", "mp3", "FLAC", "JPEG", "png", "mkv", "mp4"]
-const music_resolutions = ["Unknown kbps", "128kbps", "192kbps", "VBR", "320kbps", "CD", "Vinyl"]
+const music_resolutions = ["Unknown kbps", "64kbps", "128kbps", "192kbps", "VBR", "320kbps", "CD", "Vinyl"]
 const video_resolutions = ["SD", "720p", "1080p", "4k", "8k"]
-const pdf_resolutions = ["Digital", "Scan (Searchable)", "Scan (IMG)"]
+const pdf_resolutions = ["Digital", "Scan"]
